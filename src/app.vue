9d4a521bb3a260be510e7a1aa415d67aaea14dfb
Chenying517 <Cheny_0595@163.com>
2018-10-25 09:37:54 +0800

    18.10.25

--- src/app.vue
+++ src/app.vue
@@ -2,16 +2,19 @@
   <div id="app">
      <div id="cover"></div>
      <Header></Header>
+     <Todo></Todo>
      <Footer></Footer>
   </div>    
 </template>
 <script>
   import Header from "./todo/header.vue";
   import Footer from "./todo/footer.jsx"
+  import Todo from "./todo/todo.vue"
   export default {
-    components:{
+      components:{
       Header,
-      Footer
+      Footer,
+      Todo
     }
   }
 </script>
--- src/todo/footer.jsx
+++ src/todo/footer.jsx
@@ -1,11 +1,12 @@
 import '../assets/style/footer.styl'
+
 export default{
   data(){
     return {autor:'chenying'}
   },
   render(){
     return(
-      <div>Written by{ this.autor}</div>
+      <div id='footer'>Written by { this.autor}</div>
     )
   }
 }
